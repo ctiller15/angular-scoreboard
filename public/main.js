e5646d0d5edbfb6d4e8c4a55d55f97224b768fda
Tiller <ctiller@proforma.com>
2018-03-26 14:56:25 -0400

    add in quarter/period tracker

--- public/main.js
+++ public/main.js
@@ -7,13 +7,23 @@ angular
 
         this.teams = [];
         this.quarter = 1;
+        this.prevQuarters = [];
 
         this.updateQuarter = () => {
           this.quarter += 1;
+          let prevTeamsData = [];
           for(let i = 0; i < this.teams.length; i++) {
+            let teamObj = {
+              name: this.teams[i].name,
+              score: this.teams[i].score
+            };
             this.teams[i].resetScore();
+            prevTeamsData.push(teamObj);
           }
+          this.prevQuarters.push(prevTeamsData);
+          console.log(this);
         }
+
         this.createTeam = (teamName) => {
           let team = new Team(teamName);
           this.teams.push(team);
