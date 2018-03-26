954778f80d25e5f548d786b650d1f179299559ef
Tiller <ctiller@proforma.com>
2018-03-26 14:28:26 -0400

    add in score reset function

--- public/main.js
+++ public/main.js
@@ -1,14 +1,18 @@
 angular
   .module("mainGame", [])
   .controller("playerController", ($scope) => {
-    $scope.players = [];
 
     class Game {
       constructor(teamCount) {
+
         this.teams = [];
         this.quarter = 1;
+
         this.updateQuarter = () => {
           this.quarter += 1;
+          for(let i = 0; i < this.teams.length; i++) {
+            this.teams[i].resetScore();
+          }
         }
         this.createTeam = (teamName) => {
           let team = new Team(teamName);
@@ -25,23 +29,21 @@ angular
       constructor(name) {
         this.score = 0;
         this.name = name;
+
         this.updateScore = (val) => {
           this.score += val;
         }
+
+        this.resetScore = () => {
+          this.score = 0;
+        }
+
         this.updatedName = "";
+
         this.updateName = (name) => {
           this.name = name;
         }
       }
-    }
-
-    $scope.createTeam = (teamName) => {
-      let team = new Team(teamName);
-      $scope.players.push(team);
-    }
-
-    for(let i = 0; i < 2; i++) {
-      $scope.createTeam(`team ${i + 1}`);
     }
 
     $scope.currGame = new Game(2);
