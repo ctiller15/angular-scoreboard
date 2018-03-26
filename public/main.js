bb1e351898be2dd8de40fd2b47ef94e77de29986
Tiller <ctiller@proforma.com>
2018-03-26 13:35:00 -0400

    add in score increment/decrement buttons

--- public/main.js
+++ public/main.js
@@ -1,5 +1,32 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
+angular
+  .module("mainGame", [])
+  .controller("playerController", ($scope) => {
+    $scope.players = [];
+    $scope.teams = 2;
+    $scope.score = 10;
 
-document.addEventListener('DOMContentLoaded', main)
+    class Team {
+      constructor(name) {
+        this.score = 0;
+        this.name = name;
+        this.updateScore = (val) => {
+          this.score += val;
+        }
+        this.updateName = (name) => {
+          this.name = name;
+        }
+      }
+    }
+
+    $scope.createTeam = (teamName) => {
+      let team = new Team(teamName);
+      $scope.players.push(team);
+    }
+
+    for(let i = 0; i < 2; i++) {
+      $scope.createTeam(`team ${i + 1}`);
+    }
+
+    console.log($scope.players);
+  });
+
